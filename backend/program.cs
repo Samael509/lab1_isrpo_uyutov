dce9ca6c5548dfeb0b58d6a0196b71a22d8b57de
Samael509 <pavel718234@gmail.com>
2026-01-16 11:39:42 +0300

    test

---
+++ backend/program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Uyutov Pavel Alexsandrovich");
+Console.WriteLine(DateTime.Now);
