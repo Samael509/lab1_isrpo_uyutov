793bcc3b9b958aa6d9f5024dc19d0a1067bc5cf3
Samael509 <pavel718234@gmail.com>
2026-01-16 14:39:35 +0300

    add log

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8deef5faf4a560061d7e5c7d8d41f3273a9887e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec65856217fc164aa3a4cb91e4301b2690acbfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/program.cs
+++ backend/program.cs
@@ -1,8 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Data;
+﻿using System.Data;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Uyutov Pavel Alexsandrovich");
 Console.WriteLine(DateTime.Now);
+
+string name = Console.ReadLine();
+Console.WriteLine($"привет, {name}");
+Console.WriteLine(DateTime.Now.DayOfWeek);
+
